fb512f0908752a73be993d2e04e30faa823bed2d
Hyeladi Malgwi <46991842+H3SAN@users.noreply.github.com>
2022-06-04 14:50:24 +0300

    Finished Dashboad UI. sort of...

--- public/Js/script.js
+++ public/Js/script.js
@@ -1,21 +1,7 @@
-
-function loadHtml(id, filename) {
-    console.log(`div i: ${id}, filename: ${filename}`);
-
-    let xhttp; 
-    let element = document.getElementById(id);
-    let file = filename;
-
-    if(file){
-        xhttp = new XMLHttpRequest();
-        xhttp.pnreadystatechange = function() {
-            if(this.readyState == 4){
-                if(this.status == 200){element.innerHTML = this.responseText;}
-                if(this.status == 404){element.innerHTML = "<h1>Page not found></h1>";}
-            }
-        }
-        xhttp.open("GET",`templates/${file}`, true);
-        xhttp.send();
-        return;
-    }
-}
+const header = document.querySelector('.heading')
+fetch('../header.html')
+.then(res=>res.text())
+.then(data=>{
+    console.log('hello from console')
+    header.innerHTML = data
+})
