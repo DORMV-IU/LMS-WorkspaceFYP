029c16b0ef1a4759639efc25a109bf82b96e6829
DORMV-IU <rowlands.danielgg@gmail.com>
2022-06-10 20:10:19 +0100

    More UI Changes

--- public/Js/auth.js
+++ public/Js/auth.js
@@ -1,6 +1,10 @@
 "use strict";
+
+// console.log("duga");
 const auth = firebase.auth();
+
 const loginBtn = document.getElementById("loginBtn");
+const testName = document.getElementById("testName");
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -11,3 +15,15 @@ const provider = new firebase.auth.GoogleAuthProvider();
 const googleLogin = () => {
   auth.signInWithPopup(provider);
 };
+
+auth.onAuthStateChanged((user) => {
+  //state change monitor
+  if (user) {
+    // //signed in
+    window.location.href = "./dashboard.html";
+    console.log("signed in");
+  } else {
+    // //not signed in
+    window.location.href = "./index.html";
+  }
+});
